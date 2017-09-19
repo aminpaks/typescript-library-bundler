465901708fad4a92afbe4fae1f07bec88b66245f
aminpaks <amin.pakseresht@hotmail.com>
2017-09-18 20:54:29 -0400

    Fixes an issue with less compiler not including data-uri correctly

--- src/plugins/less-render.ts
+++ src/plugins/less-render.ts
@@ -3,13 +3,13 @@ import * as less from 'less';
 import { isNil } from '../utils';
 
 export function replaceModulesPath(content: string, currentDir: string, projectRootDir: string): string {
-  const importRE = /(@import\s*(\([^\)]+\)\s*)?(?:['"])(~))\w/gi;
+  const importRE = /(@import\s*(\([^\)]+\)\s*)?(['"])(~))(?!\3)/gi;
 
   let match: RegExpMatchArray | null;
 
   while ((match = importRE.exec(content)) && !isNil(match)) {
     const currentStatement = match[0];
-    const tilde = match[3];
+    const tilde = match[4];
     const pathToModules = path.join(path.relative(currentDir, projectRootDir), 'node_modules') + path.sep;
 
     const newStatement = currentStatement.replace(tilde, pathToModules);
@@ -26,12 +26,9 @@ export async function renderLess(content: string, currentDir: string, projectRoo
   try {
     const replacedPathsContent = replaceModulesPath(content, currentDir, projectRootDir);
     const lessResult = await less.render(replacedPathsContent, {
-      rootFileInfo: {
-        relativeUrls: true,
-        rootpath: projectRootDir,
-        currentDirectory: currentDir,
-      } as Less.RootFileInfo,
-    });
+      relativeUrls: true,
+      ieCompat: false,
+    } as Less.Options);
     result = lessResult.css;
   } catch (err) {
     throw new Error(`Less compiler ${err.message}\n\nWhile compiling => "${content}`);
